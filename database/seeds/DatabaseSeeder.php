17a7990b50fdbcb447eee8cda668cb45d12b9ef9
Tom <tomleesm@gmail.com>
2020-02-04 16:24:32 +0800

    many to many

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(App\User::class, 10)->create();
+        factory(App\Role::class, 10)->create();
+        factory(App\RoleUser::class, 20)->create();
     }
 }
