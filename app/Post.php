d67d74933f83dda37a5f57ee51986356da8501e0
Tom <tomleesm@gmail.com>
2020-04-16 17:11:19 +0800

    use /search/{query} to search

--- app/Post.php
+++ app/Post.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace App;
+use Nicolaslopezj\Searchable\SearchableTrait;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use SearchableTrait;
+
+    protected $searchable = [
+        'columns' => [
+            'title' => 1,
+            'content' => 1,
+        ]
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/search/{query}', function ($query) {
+    return \App\Post::search($query)
+               ->orderBy('id', 'asc')
+               ->get();
+});
