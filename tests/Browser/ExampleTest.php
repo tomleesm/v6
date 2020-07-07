43b7c4be4aea4fb72b6546f0086754d6bb84d516
Tom <tomleesm@gmail.com>
2020-07-08 00:51:57 +0800

    create user ramdonly

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Browser;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
@@ -26,34 +25,14 @@ class ExampleTest extends DuskTestCase
 
     public function testRefreshDatabase()
     {
-        $user = new User();
-        $user->name = 'Tom';
-        $user->email = 'tomleesm@gmail.com';
-        $user->password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
-        $user->save();
-
-        $this->assertDatabaseHas('users', [
-            'name' => 'Tom',
-            'email' => 'tomleesm@gmail.com',
-        ]);
-
+        $user = factory(User::class)->create();
         $this->assertSame(1, User::all()->count());
     }
 
     // 完全一樣的兩個測試，因爲有 RefreshDatabase trait ，所以執行完一個測試後，保證清空資料
     public function testRefreshDatabase2()
     {
-        $user = new User();
-        $user->name = 'Tom';
-        $user->email = 'tomleesm@gmail.com';
-        $user->password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
-        $user->save();
-
-        $this->assertDatabaseHas('users', [
-            'name' => 'Tom',
-            'email' => 'tomleesm@gmail.com',
-        ]);
-
+        $user = factory(User::class)->create();
         $this->assertSame(1, User::all()->count());
     }
 }
